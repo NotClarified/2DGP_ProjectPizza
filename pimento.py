027c8db83e409a0c6671b0b994484eb0814d6134
JeonDae <james911256@gmail.com>
2024-11-08 12:25:51 +0900

    피망 회전 추가

--- pimento.py
+++ pimento.py
@@ -11,11 +11,14 @@ class Pimento1:
         self.x, self.y = x, y
         self.size = size
         self.growing = True # 크기 증가 여부 조절
+        self.angle = 0  # 회전 각도 초기화
+        self.rotation_speed = 360 / 200000  # 20초 동안 360도 회전 생각해서 360 / 각도로 진행
+
     # 피망 - pimento123 :54*54
     # clip_composite_draw(self, left, bottom, width, height, rad, flip, x, y, w = None, h = None):
 
     def draw(self):
-        self.image.clip_composite_draw(0, 0, 54, 54, 0, '', self.x, self.y, self.size * 54, self.size * 54)
+        self.image.clip_composite_draw(0, 0, 54, 54, self.angle, '', self.x, self.y, self.size * 54, self.size * 54)
         print('pimento1 draw') # 확인용 주석
     def update(self):
         # 크기 조절 로직
@@ -27,7 +30,11 @@ class Pimento1:
             self.size *= 0.9999  # 크기 0.9999배 감소
             if self.size <= 0.95:  # 크기가 너무 작아지면 증가 모드로 전환
                 self.growing = True
-        self.image.clip_composite_draw(0, 0, 54, 54, 0, '', self.x, self.y, self.size * 54, self.size * 54)
+
+        self.angle += self.rotation_speed
+        if self.angle >= 360: #360도 각도에 맞춰서 360 넘기면 초기화 진행
+            self.angle -= 360
+        self.image.clip_composite_draw(0, 0, 54, 54, self.angle, '', self.x, self.y, self.size * 54, self.size * 54)
         print('pimento update')
     def handle_event(self, event):
         pass
